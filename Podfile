5f1954c6a25742bc673eab4f0ab03cc8afe754e7
Your Actual Name <Your Actual Email>
2017-10-23 22:04:08 -0400

    Firebase

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'firebase_demo' do
+
+  use_frameworks!
+
+pod 'FBSDKCoreKit'
+pod 'FBSDKShareKit'
+pod 'FBSDKLoginKit'
+
+end
---
+++ firebase_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebase_demo
+//
+//  Created by Xiaolong Li on 9/25/17.
+//  Copyright © 2017 XiaolongLi.AppLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
